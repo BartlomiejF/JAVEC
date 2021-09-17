d99ec90fb6cfec69ce9a55182b2ce92436401a17
BartlomiejF <bartlomiej.fliszkiewicz@gmail.com>
2021-09-17 09:20:14 +0200

    bugfix

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='javec',
-      version='0.1.2',
+      version='0.1.3',
       description='Just another virtual environment creator',
       url='https://github.com/BartlomiejF/JAVEC',
       author='Bartlomiej Fliszkiewicz',
       author_email='bartlomiej.fliszkiewicz.dev@gmail.com',
       license='MIT',
-      packages=['javec'],
+      packages=['javeccli'],
       zip_safe=False,
       entry_points={
-        'console_scripts': ['javec=javec.argsHandler:handler']
+        'console_scripts': ['javec=javeccli.argsHandler:handler']
         },
       long_description=long_description,
       include_package_data=True,
